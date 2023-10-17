b45263487f65514197793461c28ac9908355ddbe
Diego Pesantes <Diego.pesantes@udla.edu.ec>
2023-10-16 20:33:34 -0500

    Calculo de numero primo

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -17,6 +17,19 @@ while (numero !=0){
 }
 return 0;
 }
+
+int esPrimo (int n){
+    if (n<=1){
+        return 0;
+    }
+    for (int i=2; i*i <= n; i++){
+        if (n % 1 ==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
    
 
 
