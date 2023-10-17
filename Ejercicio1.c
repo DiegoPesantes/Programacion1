661883a4ca82b9f38f3c65972815a2f6e8e3897f
Diego Pesantes <Diego.pesantes@udla.edu.ec>
2023-10-16 20:03:54 -0500

    Creacion del bucle

---
+++ Ejercicio1.c
@@ -0,0 +1,18 @@
+//Leer dos números y mostrar todos los números terminados en 4 comprendidos entre ellos.
+#include <stdio.h>
+
+int main() {
+    int inicio, fin;
+
+    printf("Ingrese el primer número: ");
+    scanf("%d", &inicio);
+    printf("Ingrese el segundo número: ");
+    scanf("%d", &fin);
+    
+    printf("Números terminados en 4 comprendidos entre %d y %d:\n", inicio, fin);
+
+    for (int numero = inicio; numero <= fin; numero++) {
+        if (numero % 10 == 4) {
+            printf("%d\n", numero);
+        }
+    }
