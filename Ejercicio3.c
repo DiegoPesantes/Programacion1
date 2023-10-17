40d9d71ba24d9276b602aeed94b95275ef050665
Diego Pesantes <Diego.pesantes@udla.edu.ec>
2023-10-16 20:24:57 -0500

    Estructura basica

---
+++ Ejercicio3.c
@@ -0,0 +1,10 @@
+//Leer un número entero y determinar si la suma de sus dígitos es también un número primo.
+
+#include <stdio.h>
+
+int main (){
+    int numero;
+    printf ("Ingresa un numero:");
+    scanf ("%d", &numero);
+    return 0;
+}
