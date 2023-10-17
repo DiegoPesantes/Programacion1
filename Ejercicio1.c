d121c97f7e94987976a0a6b032d7a942e610a56a
Diego Pesantes <Diego.pesantes@udla.edu.ec>
2023-10-16 20:06:10 -0500

    Validacion de los datos

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -8,11 +8,22 @@ int main() {
     scanf("%d", &inicio);
     printf("Ingrese el segundo número: ");
     scanf("%d", &fin);
-    
+
     printf("Números terminados en 4 comprendidos entre %d y %d:\n", inicio, fin);
 
     for (int numero = inicio; numero <= fin; numero++) {
         if (numero % 10 == 4) {
             printf("%d\n", numero);
+        }
+    }
+
+    if (inicio > fin) {
+        printf("El primer número debe ser menor o igual al segundo número.\n");
+    } else {
+        printf("Números terminados en 4 comprendidos entre %d y %d:\n", inicio, fin);
+        for (int numero = inicio; numero <= fin; numero++) {
+            if (numero % 10 == 4) {
+                printf("%d\n", numero);
+            }
         }
     }
